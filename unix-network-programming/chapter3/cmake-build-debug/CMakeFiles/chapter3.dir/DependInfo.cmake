f927bdf1ba45b556ad14b5f96215d05f9b7267d0
HongWeisheng <2873564368@qq.com>
2019-09-17 17:59:21 +0800

    2019-09-17

--- unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/DependInfo.cmake
+++ unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/DependInfo.cmake
@@ -4,16 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/byteorder.c" "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/byteorder.c.o"
-  "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/readline1.c" "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/readline1.c.o"
-  "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/readn.c" "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/readn.c.o"
-  "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/writen.c" "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/writen.c.o"
+  "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/byteorder.c" "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/byteorder.c.o"
+  "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/readline1.c" "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/readline1.c.o"
+  "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/readn.c" "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/readn.c.o"
+  "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/writen.c" "/Users/Mac-78/Desktop/temp/network-programming/unix-network-programming/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/writen.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../lib"
+  "/Users/wison/Desktop/网络编程/unix-network-programming/chapter3/lib"
   )
 
 # Targets to which this target links.
